83d23c09fe940e49a2e557fd1b6916f1c516f843
Juliet Slade <juliet.slade@gmail.com>
2016-10-05 00:18:29 -0400

    rearranging

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ class App:
         self.canvas = Canvas(master, width=WIDTH,height=HEIGHT)
         c = self.canvas
 
+        self.matrix = [[0, 1],
+                       [1, 0]]
 
+
+        self.canvas.bind("<ButtonPress-1>", self.onMouseDown)
+        self.canvas.bind("<B1-Motion>", self.onMouseMove)
+
+        self.v1_vals = [4, 1]
+        self.v2_vals = [r[0] for r in matrixMul(self.matrix, self.v1_vals)]
 
         #Matrix entry objects
         self.a = Entry(master, bd = 5, width=3)
@@ -22,8 +30,6 @@ class App:
         self.d = Entry(master, bd = 5, width=3)
         self.d.insert(END, 0)
 
-        self.matrix = [[0, 1],
-                       [1, 0]]
 
         #Matrix grid
         self.a.grid(row=1, column=0)
@@ -36,24 +42,24 @@ class App:
         
         #Buttons
         update = Button(master, text="Update", command= lambda: self.updateMatrix(self.a.get(), self.b.get(), self.c.get(), self.d.get()))
-        update.grid(row=3)
+        update.grid(row=3, column=0)
         submit = Button(master, text="Adjoint", command = self.adjoint)
-        submit.grid(row=4)
+        submit.grid(row=3, column=1)
+
+        #Labels
         self.hermitian = Label(master, text="Hermitian")
-        self.hermitian.grid(row=5)
-
-
-    
+        self.hermitian.grid(row=5, columnspan=2)
+        self.blue = Label(master, text=str(self.v1_vals)+"^T", fg="blue", width=20)
+        self.blue.grid(row=6, columnspan=2)
+        self.red = Label(master, text=str(self.v2_vals)+"^T", fg="red", width=20)
+        self.red.grid(row=7, columnspan=2)
+        
 
         self.drawGrid()
-
-        self.canvas.bind("<ButtonPress-1>", self.onMouseDown)
-        self.canvas.bind("<B1-Motion>", self.onMouseMove)
-
-        self.v1_vals = [4, 1]
-        self.v2_vals = [r[0] for r in matrixMul(self.matrix, self.v1_vals)]
         self.v1 = self.makeVector(self.v1_vals, "blue")
         self.v2 = self.makeVector(self.v2_vals, "red")
+
+
 
 
     def updateMatrix(self, a, b, c, d):
@@ -65,7 +71,10 @@ class App:
         else:
             self.hermitian.grid_remove()
         
-                                
+    def updateCoords(self):
+        self.blue['text'] = str([round(x, 2) for x in self.v1_vals])+"^T"
+        self.red['text'] = str([round(x, 2) for x in self.v2_vals])+"^T"
+                               
     def adjoint(self):
         b_value = self.b.get()
         c_value = self.c.get()
@@ -81,6 +90,7 @@ class App:
     def isHermitian(self):
         return self.getCurrentMatrix() == [[self.a.get(), self.c.get()],[self.b.get(), self.d.get()]]
 
+    def unitary(
 
     def onMouseDown(self, event):
         self.moveV1(event.x, event.y)
@@ -122,6 +132,7 @@ class App:
             HEIGHT/2 + -self.v2_vals[1]*UNIT_SIZE
         )
         self.updateCoords()
+
 
 root = Tk()
 all = App(root)
